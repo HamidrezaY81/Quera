1b66321a0907fc43f29074491637e4933a9195ad
HamidMolareza <molareza@protonmail.com>
2024-11-07 12:23:01 +0330

    fix(template): update csharp template to net8.

--- Templates/csharp/Program.cs
+++ Templates/csharp/Program.cs
@@ -2,39 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Quera {
-    public static class Program {
-        public static void Main() {
-            //TODO: Use Solution class
+namespace Quera;
+
+public static class Solution {
+    //TODO: Complete this section
+}
+
+public static class Program {
+    public static void Main() {
+        //TODO: Use Solution class
+    }
+
+    private static List<T> GetInputs<T>(int count) {
+        var result = new List<T>(count);
+        for (var i = 0; i < count; i++) {
+            var item = Convert.ChangeType(Console.ReadLine(), typeof(T)) ?? throw new ArgumentException(
+                $"Can not convert the input of the {i + 1}th line to the requested type ({typeof(T)}).");
+            result.Add((T)item);
         }
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
-            for (var i = 0; i < count; i++) {
-                var item = Convert.ChangeType(Console.ReadLine(), typeof(T)) ?? throw new ArgumentException(
-                    $"Can not convert the input of the {i + 1}th line to the requested type ({typeof(T)}).");
-                result.Add((T) item);
-            }
-
-            return result;
-        }
-
-        private static List<T> GetInputs<T>(char separator) =>
-            Console.ReadLine()
-                ?.Trim()
-                .Split(separator)
-                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList()
-            ?? new List<T>();
-
-        private static void PrintLine<T>(this T source) => Console.WriteLine(source);
-
-        private static void PrintLine<TSource, TResult>(
-            this TSource source,
-            Func<TSource, TResult> func
-        ) => Console.WriteLine(func(source));
+        return result;
     }
-    
-    public static class Solution {
-        //TODO: Complete this section
-    }
+
+    private static List<T> GetInputs<T>(char separator) =>
+        Console.ReadLine()
+            ?.Trim()
+            .Split(separator)
+            .Select(item => (T)Convert.ChangeType(item, typeof(T))).ToList()
+        ?? [];
+
+    private static void PrintLine<T>(this T source) => Console.WriteLine(source);
+
+    private static void PrintLine<TSource, TResult>(
+        this TSource source,
+        Func<TSource, TResult> func
+    ) => Console.WriteLine(func(source));
 }
